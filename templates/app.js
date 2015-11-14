35d60644548a4dec3252d46f0bef54a553226e03
JannaShaftan <shaftan.j@husky.neu.edu>
2015-11-14 17:48:59 -0500

    angular routing & form

---
+++ templates/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('RCapp', [
+  'ui.router']);
+
+app.controller('qCtrl', ["$scope", function($scope){
+  $scope.master = {};
+
+  $scope.update = function(user){
+    $scope.master = angular.copy(user);
+  }
+
+  $scope.reset();
+
+}]);
+
+app.config(function($stateProvider, $urlRouterProvider){
+
+$urlRouterProvider.otherwise('/index');
+  $stateProvider
+
+  .state('questions',
+  {
+    url:'/index',
+    templateUrl:'index.html'
+  })
+
+  .state('results',
+  {
+    url:'/results',
+    templateUrl:'results.html'
+  })
+});
+
